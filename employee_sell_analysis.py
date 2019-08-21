718f76ca444bdb6426ebece1d2ebbe19e2fc7d26
nakulmehta <54034735+nakulmehta@users.noreply.github.com>
2019-08-21 21:32:02 +0530

    Update employee_sell_analysis.py

--- employee_sell_analysis.py
+++ employee_sell_analysis.py
@@ -26,24 +26,29 @@ class month_sell :
             return (self.sellA+self.sellB+self.sellC)
 
 print ("enter the sells of employe1:")
+
 sell1 = int (input("enter the sell of first month :")) 
 sell2 = int (input("enter the sell of second month :"))
 sell3 = int(input("enter the sell of third month : "))
 
 print ("enter the sells of employe2 :")
+
 sell4 = int (input("enter the sell of first month : "))
 sell5 = int(input("enter the sell of second month : "))
 sell6 = int(input("enter the sell of third month : "))
 
 print ("enter the sell of employe3:")
+
 sell7 = int (input("enter the sell of first month:"))
 sell8 = int (input("enter the sell of second month :"))
 sell9 = int (input("enter the sell of third month :"))
 
 employe1 = month_sell(sell1 , sell2 , sell3)
 employe1.month_graph()
+
 employe2 = month_sell(sell4 , sell5 , sell6)
 employe2.month_graph()
+
 employe3 = month_sell(sell7 , sell8 , sell9)
 employe3.month_graph()
 
@@ -53,8 +58,10 @@ if command1 == 'yes' :
 
     employe1 = month_sell(sell1, sell2 , sell3)
     user1 = employe1.add()
+    
     employe2 = month_sell(sell4, sell5 , sell6)
     user2 = employe2.add()
+    
     employe3 = month_sell(sell7 , sell8, sell9)
     user3 = employe3.add()
 
